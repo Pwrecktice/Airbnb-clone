e8715d054e8621719c426d8b7130a4b44e279769
xuchunlei3 <xuchunlei3@wanda.cn>
2021-02-18 15:18:39 +1300

    finish post

--- App.js
+++ App.js
@@ -7,23 +7,24 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
-import HomeScreen from './src/screens/Home'
+import {SafeAreaView, StatusBar} from 'react-native';
+import HomeScreen from './src/screens/Home';
+import Post from './src/components/Post';
+
+import feed from './assets/data/feed';
+
+const data = feed[0];
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <HomeScreen />
+        {/* <HomeScreen /> */}
+        <Post data={data} />
       </SafeAreaView>
     </>
   );
 };
-
-
 
 export default App;
---
+++ src/components/Post/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import styles from './styles';
+
+const Post = (props) => {
+  const {data} = props;
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: data.image,
+        }}
+      />
+      <Text style={styles.bedrooms}>
+        {data.bed} bed {data.bedroom} bedroom
+      </Text>
+      {/* Type & Description */}
+      <Text style={styles.description} numberOfLines={2}>
+        {data.type}. {data.title}
+      </Text>
+      {/* Old price & new price */}
+      <Text style={styles.prices}>
+        <Text style={styles.oldPrice}>${data.oldPrice}</Text>
+        <Text style={styles.price}> ${data.newPrice} </Text>/ night
+      </Text>
+      {/* Total price */}
+      <Text style={styles.totalPrice}>${data.totalPrice} total</Text>
+    </View>
+  );
+};
+
+export default Post;
